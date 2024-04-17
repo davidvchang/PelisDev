1719b03c859060b8db74c46a3aa2ff558f4838cc
David Valenzuela <dvalenzuelachang@gmail.com>
2024-04-16 22:38:18 -0700

    Inicio del proyecto e instalacíon de Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/NavBar"
+
+function App() {
+
+  return (
+    <main className="w-full h-screen">
+      <NavBar/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <section className='w-full h-20'>
+      
+    </section>
+  )
+}
+
+export default NavBar
